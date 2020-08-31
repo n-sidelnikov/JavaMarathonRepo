a0ad7f02bb8e08580a5a5a546a66b0ddd9ee7815
Nikolay Sidelnikov <kolya.trudovik@gmail.com>
2020-08-31 23:46:54 +0300

    add solution Task2

--- src/day8/Task2.java
+++ src/day8/Task2.java
@@ -1,2 +1,11 @@
-package day8;public class Task2 {
+package day8;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Plane plane = new Plane("England", 2010, 50, 1000);
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("Изготовитель: " + plane.getProducer()).append(", год выпуска: ").append(plane.getYearOfIssue()).append(", длина: ").append(plane.getLength()).append(", вес: ").append(plane.getWeight()).append(", обьем топлива в баке: ").append(plane.getFuelOfVolume());
+        System.out.println(builder.toString());
+    }
 }
